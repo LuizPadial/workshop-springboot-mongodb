642ce04410189b81ac278d4eba4d48c3a69cd961
luizPadial <luizpadial10@gmail.com>
2024-03-27 22:49:51 -0300

    Inserção de Usuário com POST

--- src/main/java/com/br/SpringMongoDb/curso/services/UserService.java
+++ src/main/java/com/br/SpringMongoDb/curso/services/UserService.java
@@ -1,6 +1,7 @@
 package com.br.SpringMongoDb.curso.services;
 
 import com.br.SpringMongoDb.curso.domain.User;
+import com.br.SpringMongoDb.curso.dto.UserDTO;
 import com.br.SpringMongoDb.curso.repository.UserRepository;
 import com.br.SpringMongoDb.curso.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
